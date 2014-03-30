f59e05a4ada5c1502bf867b3f4a7c0d4c4e3f2f5
Mason Fabel <mason.fabel@gmail.com>
2014-03-29 21:32:21 -0700

    Added project header
    Added a header describing the project to the top of the source file.

--- hw04.asm
+++ hw04.asm
@@ -1,3 +1,11 @@
+; Mason Fabel
+; 29 March 14
+; CS 150
+; Homework 4
+;
+; Count the number of 1 bits in R0
+; and store the result in R7
+;
 	.ORIG x3040
 INIT	AND R1, R1, x00
 	ADD R1, R1, X01
@@ -6,5 +14,5 @@ LOOP	AND R2, R0, R1
 	ADD R7, R7, R2
 	ADD R1, R1, R1
 	BRzp LOOP
-	HALT
+END	HALT
 	.END
